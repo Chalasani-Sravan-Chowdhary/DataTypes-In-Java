2928a7c3dabd45eae46da5c6e8d437802283ca39
Chalasani Sravan Chowdhary <csc.education.211124@gmail.com>
2025-01-28 09:58:24 +0530

    Add files via upload

---
+++ DataTypes.java
@@ -0,0 +1,20 @@
+public class DataTypes {
+    public static void main(String[] args){
+        byte bytevar=60;
+        short shortvar=300;
+        int intvar=3000;
+        long longvar=500L;
+        float floatvar=50.05f;
+        double doublevar=30;
+        char charvar='C';
+        String G="GITHUB";
+        System.out.println("Byte value :" + bytevar);
+        System.out.println("Short value :" + shortvar);
+        System.out.println("int value :" + intvar);
+        System.out.println("Long value :" + longvar);
+        System.out.println("Float value :" + floatvar);
+        System.out.println("Double value :" + doublevar);
+        System.out.println("Char value :" + charvar);
+        System.out.println("String value :" + G);
+    }
+}
